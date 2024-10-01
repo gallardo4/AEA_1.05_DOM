573c9a3b4a1f57265311fad2c7df63de597bb680
arnau <arnau.gallardo.7e7@itb.cat>
2024-10-02 00:33:49 +0200

    Commit

--- dom.js
+++ dom.js
@@ -17,8 +17,37 @@ btnCanvi.onclick = function() {
 // Exercici 2
 
 const div = document.createElement("div");
+div.innerHTML = "Div amb text clicable.";
+div.style.fontSize = "16px";
+div.style.color = "black";
+
 const body = document.getElementById("body");
 body.appendChild(div);
+
 div.onclick = function() {
-    
+    div.style.color = "blue";
+    div.style.fontSize = "24px";
+}
+
+// Exercici 3
+
+const form = document.createElement("form");
+const input = document.createElement("input");
+input.type = "text";
+input.placeholder = "Escriu aquí...";
+const submitBtn = document.createElement("button");
+submitBtn.innerHTML = "Envia";
+
+form.appendChild(input);
+form.appendChild(submitBtn);
+body.appendChild(form);
+
+const resultat = document.createElement("p");
+body.appendChild(resultat);
+
+form.onsubmit = function() {
+    event.preventDefault();
+    const valorInput = input.value;
+    resultat.innerHTML = valorInput;
+    input.value = "";
 }
