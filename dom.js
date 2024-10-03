0a9f7c155e790c1b507674802386ab5cd0f85e5c
gallardo4 <arnau.gallardo.7e7@itb.cat>
2024-10-03 17:42:09 +0200

    Commit

--- dom.js
+++ dom.js
@@ -31,23 +31,79 @@ div.onclick = function() {
 
 // Exercici 3
 
+const llistaEsdeveniments = document.createElement("h2");
+llistaEsdeveniments.innerHTML = "Llista d'esdeveniments";
+
 const form = document.createElement("form");
+
 const input = document.createElement("input");
 input.type = "text";
 input.placeholder = "Escriu aquí...";
+
 const submitBtn = document.createElement("button");
 submitBtn.innerHTML = "Envia";
 
 form.appendChild(input);
 form.appendChild(submitBtn);
+body.appendChild(llistaEsdeveniments);
 body.appendChild(form);
 
 const resultat = document.createElement("p");
 body.appendChild(resultat);
 
-form.onsubmit = function() {
+form.onsubmit = function(event) {
     event.preventDefault();
     const valorInput = input.value;
     resultat.innerHTML = valorInput;
     input.value = "";
+}
+
+// Exercici 4
+
+const llistaSuggeriments = document.createElement("h2");
+llistaSuggeriments.innerHTML = "Llista de suggeriments"
+
+const form2 = document.createElement("form");
+
+const input2 = document.createElement("input");
+input2.type = "text";
+input2.placeholder = "Afegeix un suggeriment...";
+
+const submitBtn2 = document.createElement("button");
+submitBtn2.innerHTML = "Afegeix";
+
+form2.appendChild(input2);
+form2.appendChild(submitBtn2);
+body.appendChild(llistaSuggeriments);
+body.appendChild(form2);
+
+const suggerimentList = document.createElement("ul");
+body.appendChild(suggerimentList);
+
+form2.onsubmit = function(event) {
+    event.preventDefault();
+
+    const valorInput2 = input2.value;
+
+    const suggeriment = document.createElement("li");
+    suggeriment.innerHTML = valorInput2;
+
+    const fetBtn = document.createElement("button");
+    fetBtn.innerHTML = "Marca com a realitzat";
+    fetBtn.onclick = function() {
+        suggeriment.style.textDecoration = "line-through";
+    }
+
+    const eliminarBtn = document.createElement("button");
+    eliminarBtn.innerHTML = "Eliminar";
+    eliminarBtn.onclick = function() {
+        suggerimentList.removeChild(suggeriment);
+    }
+
+    suggeriment.appendChild(fetBtn);
+    suggeriment.appendChild(eliminarBtn);
+
+    suggerimentList.appendChild(suggeriment);
+
+    input2.value = "";
 }
